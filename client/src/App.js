3477860354709c223327609bbdf896a8ef9d17b7
browe83 <rowe.brandon.j@gmail.com>
2020-12-15 15:15:28 -0500

    adds Courses component

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  const [courses, setCourses ] = useState([]);
+import Header from './components/Header';
+import Courses from './components/Courses';
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/courses')
-      .then(response => response.json())
-      .then(data => {
-        setCourses(data.filteredCoursesInfo); 
-        console.log(data);
-      });
-  }, []);
-
-// console.log(courses);
-  const courseTitles = courses.map(course => <li>{course.course.title}</li>);
-
+function App () {
   return (
-    <div className="App">
-      <h1>hi</h1>
-        <ul>
-          {courseTitles}
-        </ul>
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        <Switch>
+          <Route path='/courses' component={Courses}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+// function App() {
+//   const [courses, setCourses ] = useState([]);
+
+//   useEffect(() => {
+//     fetch('http://127.0.0.1:5000/api/courses')
+//       .then(response => response.json())
+//       .then(data => {
+//         setCourses(data.filteredCoursesInfo); 
+//         console.log(data);
+//       });
+//   }, []);
+
+// // console.log(courses);
+//   const courseTitles = courses.map(course => <li>{course.course.title}</li>);
+
+//   return (
+//     <div className="App">
+//       <h1>hi</h1>
+//         <ul>
+//           {courseTitles}
+//         </ul>
+//     </div>
+//   );
+// }
 
 export default App;
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,2 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+function Courses (props) {
+  const [courses, setCourses ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/api/courses')
+      .then(response => response.json())
+      .then(data => {
+        setCourses(data.filteredCoursesInfo); 
+        console.log(data.filteredCoursesInfo);
+      });
+  }, []);
+
+  const courseLinks = courses.map(({ course }) => {
+      return (
+            <div className="grid-33"><a className="course--module course--link" href={`/courseDetails/${course.id}`}>
+                <h4 className="course--label">Course</h4>
+                <h3 className="course--title">{course.title}</h3>
+            </a></div>
+      );
+  })
+    return (
+        <div className="bounds">
+            {courseLinks}
+            <div className="grid-33"><a className="course--module course--add--module" href="create-course.html">
+                <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 13 13" className="add">
+                    <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                </svg>New Course</h3>
+            </a></div>
+        </div>
+    );
+};
+
+export default Courses;
